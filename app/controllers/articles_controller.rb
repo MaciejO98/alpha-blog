4ad57c1495a1cda1f6e71e088d6708945348ab6c
Maciekgit config --global user.name Maciekgit config --global user.name Maciek <mopalskilowicz@gmail.com>
2021-02-11 17:41:11 +0100

    add update posibility

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,11 @@ class ArticlesController < ApplicationController
         @article = Article.new 
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+
     def create 
         @article = Article.new(params.require(:article).permit(:title, :description))
          if @article.save
@@ -20,5 +25,15 @@ class ArticlesController < ApplicationController
          else
             render 'new'
          end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated succesfully"
+            redirect_to @article
+        else
+            render 'edit'
+        end
     end
 end
